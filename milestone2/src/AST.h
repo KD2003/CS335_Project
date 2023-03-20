0d3dc4eab056907ecd543a7260fa96354e138ead
Kartavya <betudamor@gmail.com>
2023-03-20 20:40:15 +0530

    typecheck

--- milestone2/src/AST.h
+++ milestone2/src/AST.h
@@ -16,6 +16,7 @@ typedef struct{
 	int is_error = 0;
 
 	string temp_name;
+	vector<int> dimension;
 	// qid place;
 }ASTNode;
 
--- milestone2/src/typecheck.cpp
+++ milestone2/src/typecheck.cpp
@@ -1,9 +1,8 @@
 #include "typecheck.h"
 
-//RETURNS THE TYPE OF IDENTIFIER id USING lookup
 string primaryExpression(string id) {
     sym_entry* n = lookup(id);
-    if(n) {
+    if(n){
         string s = n->type;
         return s;     
     }
@@ -17,24 +16,12 @@ string primaryExpression(string id) {
 string postfixExpression(string type_name, int rule_num) {
     string result_type;
     switch(rule_num) {
-        case 1: {
-            if(type_name.back()=='*')return type_name.substr(0,type_name.length()-1);
-            return "";
-        }
-        case 2:{
+        case 1:{
             if(type_name.substr(0, 5)=="FUNC_")return type_name.substr(5, type_name.length() - 5); 
             return "";
         }
-        case 3:{
-            if(type_name.substr(0, 5)=="FUNC_")return type_name.substr(5, type_name.length() - 5);
-            return "";
-        }
-        case 6:{
-            if(isInt(type_name) || type_name == "char")return type_name;
-            return "";
-        }
-        case 7:{
-            if(isInt(type_name) || type_name == "char")return type_name;
+        case 2:{
+            if(isInt(type_name) || isFloat(type_name))return type_name;
             return "";
         }
         default : return "";
@@ -42,31 +29,14 @@ string postfixExpression(string type_name, int rule_num) {
 }
 
 string checkType(string a, string b){
-    if(a == b)return "ok";
-    if((a == "void*" && b.back()=='*')||(a.back()=='*' && b == "void"))return "ok";
-    if(a.back()=='*' && b.back()=='*')return "warning";
-    if((isInt(a) && b.back()=='*')||(isInt(b)&&a.back()=='*'))return "warning";
-    if(a == "char" || isInt(a)) a = "long double";
-    if(b == "char" || isInt(b)) b = "long double";
-    if(isFloat(a) && isFloat(b)) return "ok";
+    if(a == b)return a;
+    if(isInt(b)) return a;
+    if(isFloat(a) && isFloat(b)) return a;
     return "";
 }
 
-string argExp(string a, string b, int rule_num){
-    if(rule_num == 1){
-        if(a == "void") return a;
-        return "";
-    }
-    else{
-        if(a == "void" && b=="void") return a;
-        return "";
-    }
-}
-
-string unaryExp(string op, string type){
-    if(op=="*") return postfixExpression(type, 1);
-    else if(op == "&") type+="*";
-    else if(op=="+" || op=="-"){
+string unaryExp(string type, string op){
+    if(op=="+" || op=="-"){
         if(!(isInt(type) || isFloat(type))) return "";
     }
     else if(op=="~" && !isInt(type)){
@@ -76,25 +46,12 @@ string unaryExp(string op, string type){
     return type;
 }
 
-string mulExp(string a, string b, char op){
-    if(op=='*' || op =='/'){
-        if(isInt(a) && isInt(b)) return "int";
-        else if((isInt(a) || isFloat(a)) && (isInt(b) || isFloat(b))) return "float";
-        return "";
-    }
-    else if(op=='%'){
-        if(isInt(a) && isInt(b)) return "int";
-    }
-    return "";
+string mulExp(string a, string b){
+    return promotedType(a, b);
 }
 
-string addExp(string a, string b, char op){
-    if(isInt(a) && isInt(b)) return "int";
-    else if((isInt(a) || isFloat(a)) && (isInt(b) || isFloat(b))) return "real";
-    else if((isInt(a) && b=="char") || (a=="char" && isInt(b))) return "char";
-    else if(isInt(a) && b.back()=='*') return b;
-    else if(a.back()=='*' && isInt(b)) return a;
-    return "";
+string addExp(string a, string b){
+    return promotedType(a,b);
 }
 
 string shiftExp(string a, string b){
@@ -103,22 +60,18 @@ string shiftExp(string a, string b){
 }
 
 string relExp(string a, string b){
-    if((isInt(a) || isFloat(a) || a=="char") && (isInt(b) || isFloat(b) ||b=="char")) return "boolean";
-    if((isInt(a) || a=="char") && b.back()=='*') return "Boolean";
-    if((isInt(b) || b=="char") && a.back()=='*') return "Boolean";
+    if((isInt(a) || isFloat(a)) && (isInt(b) || isFloat(b))) return "boolean";
     return "";
 }
 
 string eqExp(string a, string b){
-    if(a==b) return "Ok";
-    if((isInt(a) || isFloat(a) || a=="char") && (isInt(b) || isFloat(b) ||b=="char")) return "Ok";
-    if((isInt(a) && b.back()=='*') || (a.back()=='*' && isInt(b))) return "ok";
+    if((isInt(a) || isFloat(a) || a=="boolean") && (isInt(b) || isFloat(b) ||b=="boolean")) return "boolean";
     return "";
 }
 
 string bitExp(string a, string b){
-    if(a=="boolean" && b=="boolean") return "ok";
-    if((a=="boolean" || isInt(a)) && (b=="boolean" || isInt(b))) return "Ok";
+    if(a=="boolean" && b=="boolean") return "boolean";
+    if(isInt(a) && isInt(b)) return promotedType(a,b);
     return "";
 }
 
@@ -127,14 +80,14 @@ string assignExp(string a, string b, string op){
         return checkType(a,b);
     }
     if(op == "*=" || op == "/=" || op == "%="){
-        if(mulExp(a, b, op[0])=="") return "";
+        if(mulExp(a, b)=="") return "";
         else return "ok";
     }
     if(op == "+=" || op == "-="){
-        if(addExp(a, b, op[0])=="") return "";
+        if(addExp(a, b)=="") return "";
         else return "ok";
     }
-    if(op == ">>=" || op == "<<="){
+    if(op == ">>=" || op == "<<=" || ">>>="){
         if(shiftExp(a, b)=="") return "";
         else return "ok";
     }
@@ -145,12 +98,23 @@ string assignExp(string a, string b, string op){
     return "";
 }
 
-string condExp(string a,string b){
-    if(a == b)return a;
-    if(a == "char"|| isInt(a)) a = "float";
-    if(b == "char"|| isInt(b)) b = "float";
-    if(isFloat(a) && isFloat(b)) return "int";
-    if(a.back() == '*' && b.back() == '*')return "void*" ;
+string condExp(string a,string b, string c,string op){
+    if(op == "&&" | op== "||"){
+        if(a == "boolean" && b=="boolean") return "boolean";
+        return "";
+    }
+    if(a == "boolean" && b!="void" && c!= "void"){
+        if(a==b) return a;
+        return promotedType(a,b);
+    }
+    return "";
+}
+
+string promotedType(string a, string b){
+    if(a == "double" || b=="double") return "double";
+    if(a == "float" || b=="float") return "float";
+    if(a == "long" || b=="long") return "long";
+    if(isInt(a) || isInt(b)) return "int";
     return "";
 }
 
@@ -170,7 +134,6 @@ bool isFloat(string type){
 }
 
 bool isVoid(string type){
-    if(type.length() < 4) return 0;
-    else if(type.substr(0,4) == "void") return 1;
-    else return 0;
+    if(type == "void") return 1;
+    return 0;
 }
