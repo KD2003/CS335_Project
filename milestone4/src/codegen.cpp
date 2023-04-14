6edcdedb0285dfd4298f539e90c133f4c977eac4
KD <betudamor@gmail.com>
2023-04-14 14:15:33 +0530

    codegen

---
+++ milestone4/src/codegen.cpp
@@ -0,0 +1,4 @@
+#include "codegen.h"
+
+int string_counter = 0;
+int label_counter = 0;
---
+++ milestone4/src/codegen.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <bits/stdc++.h>
+#include "3ac.h"
+using namespace std;
+
+
+
+void add_op(quad* instr);
+void sub_op(quad* instr);
+void mul_op(quad* instr);
+void div_op(quad* instr);
+void mod_op(quad* instr);
+void bitwise_or_op(quad* instr);
+void bitwise_and_op(quad* instr);
+void return_op(quad* instr);
+void clear_regs();
+void call_func(quad &instr);
+void add_op(quad* instr);
+void assign_op(quad* instr);
+void comparison_op(quad* instr, string op);
+void logic_and(quad* instr);
+void logic_or(quad* instr);
+void bitwise_op(quad* instr);
+void unary_op(quad* instr);
+void pointer_op(quad* instr);
+void free_reg(string reg);
+void ptr_op(quad* instr);
+void member_access(quad* instr);
+void array_op(quad* instr);
+void goto_op(quad* instr);
